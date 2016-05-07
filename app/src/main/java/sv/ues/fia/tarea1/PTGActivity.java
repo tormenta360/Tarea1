ed525726d86cd660fcc49966de98d8298b5b820b
tormenta360 <pictigrito@hotmail.com>
2016-05-07 15:39:25 -0600

    Initial commit

---
+++ app/src/main/java/sv/ues/fia/tarea1/PTGActivity.java
@@ -0,0 +1,13 @@
+package sv.ues.fia.tarea1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PTGActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ptg);
+    }
+}
