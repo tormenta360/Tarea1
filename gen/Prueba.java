4552e8dd6ac643a9fb483466af43b5d93af64492
AyalaFilip <fkayala@gmail.com>
2016-05-08 23:58:46 -0600

    prueba 1.0

--- gen/Prueba.java
+++ gen/Prueba.java
@@ -2,4 +2,13 @@
  * Created by User on 08/05/2016.
  */
 public class Prueba {
+    String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
